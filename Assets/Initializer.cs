7fca4ba7e1c2c27c296a48cd14d78cd30b142fe4
SimonMarkWhittle <smw2ed@virginia.edu>
2017-05-03 02:27:43 -0400

    Manors Mostly(?) done. Coming along nicely.

---
+++ Assets/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initializer : MonoBehaviour {
+
+    public ManorGenerator mG;
+
+	// Use this for initialization
+	void Start () {
+        mG.init();
+	}
+}
